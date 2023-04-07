e07a8cf473638f2d7433297a2e7ee0a3b97cfb11
enrique.torres <etsmsj@gmail.com>
2023-04-06 21:41:41 -0400

    network initialization script working

--- initialize_network.sh
+++ initialize_network.sh
@@ -1,18 +1,19 @@
 cd ~/ECE1770_Group4_Project/fabric-samples/test-network
-./network.sh down
 ./network.sh up createChannel -c channel1 -ca
 
 peer lifecycle chaincode package basic.tar.gz --path ~/ECE1770_Group4_Project/fabric-samples/asset-transfer-basic/chaincode-javascript-consensus-project/ --lang node --label basic_1.0
 . ./scripts/envVar.sh
 setGlobals 1
+echo "Installing on node 1... This might take some time..."
 peer lifecycle chaincode install basic.tar.gz
 setGlobals 2
+echo "Installing on node 2... This might take some time..."
 peer lifecycle chaincode install basic.tar.gz
 setGlobals 1
-peer lifecycle chaincode queryinstalled --peerAddresses localhost:7051 --tlsRootCertFiles organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-
+output=$(peer lifecycle chaincode queryinstalled --peerAddresses localhost:7051 --tlsRootCertFiles organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt)
+package_id=$(echo "$output" | grep -oP 'Package ID: \K[^,]+')
 #the PKGID below should match the output you got from previous command this ID is different everytime
-export PKGID=basic_1.0:1aa1cf5818c12a8f35031f98ab18785cb0a0b17174f1ee6af587a6a670a4f761
+export PKGID=$package_id
 
 peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $PWD/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID channel1 --name basic --version 1 --package-id $PKGID --sequence 1
 
